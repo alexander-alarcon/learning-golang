08007879804e95e836663882c77e1f216b809bca
Alexander Alarcon <walarcon14@gmail.com>
2025-08-31 19:03:13 -0500

    green: implement function Perimeter
    - All tests pass

---
+++ shapes/shapes.go
@@ -0,0 +1,24 @@
+// Package shapes provides shapes functions related to area and perimeter
+package shapes
+
+import "math"
+
+type PerimeterOptions struct {
+	Width  float64
+	Height float64
+}
+
+// Perimeter returns the perimeter of a rectangle
+// rules:
+// - if any dimension is equal to 0, return -1
+// - if any dimension is negative, convert it to positive
+func Perimeter(options PerimeterOptions) float64 {
+	if options.Width == 0 || options.Height == 0 {
+		return -1.0
+	}
+
+	width := math.Abs(options.Width)
+	height := math.Abs(options.Height)
+
+	return 2 * (width + height)
+}
