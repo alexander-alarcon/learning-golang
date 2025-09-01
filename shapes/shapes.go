ef574967fe455bf9d224db2cab036342324e5e9c
Alexander Alarcon <walarcon14@gmail.com>
2025-08-31 19:15:49 -0500

    green: implement Area function
    - All tests pass

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -22,3 +22,18 @@ func Perimeter(options PerimeterOptions) float64 {
 
 	return 2 * (width + height)
 }
+
+// Area returns the area of a rectangle
+// rules:
+// - if any dimension is equal to 0, return -1
+// - if any dimension is negative, convert it to positive
+func Area(options PerimeterOptions) float64 {
+	if options.Width == 0 || options.Height == 0 {
+		return -1.0
+	}
+
+	width := math.Abs(options.Width)
+	height := math.Abs(options.Height)
+
+	return width * height
+}
