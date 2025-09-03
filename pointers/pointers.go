a60f615fbcb10974259869208fdd940215d36dde
Alexander Alarcon <walarcon14@gmail.com>
2025-09-02 21:59:10 -0500

    green: implement deposit validation to reject non-positive amounts
    - All tests pass

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -9,8 +9,13 @@ type Wallet struct {
 	balance Bitcoin
 }
 
-func (wallet *Wallet) Deposit(amount Bitcoin) {
+func (wallet *Wallet) Deposit(amount Bitcoin) error {
+	if amount <= 0 {
+		return errors.New("deposit amount must be greater than zero")
+	}
+
 	wallet.balance += amount
+	return nil
 }
 
 func (wallet *Wallet) Balance() Bitcoin {
